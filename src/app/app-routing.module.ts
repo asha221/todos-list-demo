f9c45b7e9a87aae98c467b91de501f36ce9f30f8
asha <shaikasha221@gmail.com>
2021-04-03 12:15:41 +0530

    rounting add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddEditTodoComponent } from './todos/add-edit-todo/add-edit-todo.component';
 import { TodosListComponent } from './todos/todos-list/todos-list.component';
 
 const routes: Routes = [
-  {path:'list', component : TodosListComponent}
+  {path:'list', component : TodosListComponent},
+  {path:'add-edit', component: AddEditTodoComponent}
 ];
 
 @NgModule({
